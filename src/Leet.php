78ff4b23bc8611a19b86a978f127f864c68912a7
Blake Scott and Jason Wuerch <blakerscott@gmail.com>
2016-02-15 14:19:19 -0800

    pass last spec

--- src/Leet.php
+++ src/Leet.php
@@ -5,10 +5,27 @@
 		 {
 
 
-			 $result = str_replace("e","3", $input);
-			 return $result;
-		 		 }
-
+			 $input = str_replace("e","3", $input);
+			 $input = str_replace("E","3", $input);
+			 $input = str_replace("o","0", $input);
+			 $input = str_replace("I", "1", $input);
+			 $input = str_replace("O","0", $input);
+			 $array = explode(" ", $input);
+			 $blank_array = array();
+				 foreach ($array as $word) {
+					 $i = 0;
+					 while ($i < strlen($word)) {
+						 if ($i > 0 && $word[$i] == "s") {
+							 $word[$i] = "z";
+						 }
+						 $i++;
+					 }
+					 array_push($blank_array, $word);
+				 }
+				 $new = implode(" ", $blank_array);
+		     return $new;
+	 }
 	}
 
 ?>
+[]
--- tests/ClassTest.php
+++ tests/ClassTest.php
@@ -9,26 +9,93 @@
 		{
 		//Arrange
 		$test_LeetGenerator = new Leet;
-		$input = 'beowulf';
+		$input = 'a';
 
 		//Act
 		$result = $test_LeetGenerator->makeLeet($input);
 
 		//Assert
-		$this->assertEquals('beowulf', $result);
+		$this->assertEquals('a', $result);
 		}
+
+		function test_print_Multiple_Words()
+		{
+		//Arrange
+		$test_LeetGenerator = new Leet;
+		$input = 'a yak';
+
+		//Act
+		$result = $test_LeetGenerator->makeLeet($input);
+
+		//Assert
+		$this->assertEquals('a yak', $result);
+		}
+
 
 		function test_makeLeet3()
 		{
 		//Arrange
 		$test_LeetGenerator = new Leet;
-		$input = 'beowolf eats eggs';
+		$input = 'egg eat';
 
 		//Act
 		$result = $test_LeetGenerator->makeLeet($input);
 
 		//Assert
-		$this->assertEquals('b3owolf 3ats 3ggs', $result);
+		$this->assertEquals('3gg 3at', $result);
+		}
+		//
+		//
+		function test_makeLeet3_0()
+		{
+		//Arrange
+		$test_LeetGenerator = new Leet;
+		$input = 'dog eat';
+
+		//Act
+		$result = $test_LeetGenerator->makeLeet($input);
+
+		//Assert
+		$this->assertEquals('d0g 3at', $result);
+		}
+
+		function test_makeLeet3_0_Caps()
+		{
+		//Arrange
+		$test_LeetGenerator = new Leet;
+		$input = 'dOg Eat';
+
+		//Act
+		$result = $test_LeetGenerator->makeLeet($input);
+
+		//Assert
+		$this->assertEquals('d0g 3at', $result);
+		}
+
+		function test_makeLeet3_0_Caps_I()
+		{
+		//Arrange
+		$test_LeetGenerator = new Leet;
+		$input = 'dog Eat I am i';
+
+		//Act
+		$result = $test_LeetGenerator->makeLeet($input);
+
+		//Assert
+		$this->assertEquals('d0g 3at 1 am i', $result);
+		}
+
+		function test_makeLeet3_0_Caps_I_S()
+		{
+		//Arrange
+		$test_LeetGenerator = new Leet;
+		$input = 'dog Eat I am i sosks';
+
+		//Act
+		$result = $test_LeetGenerator->makeLeet($input);
+
+		//Assert
+		$this->assertEquals('d0g 3at 1 am i s0zkz', $result);
 		}
 
 
